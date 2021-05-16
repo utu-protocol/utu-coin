68b30f372ef6b6da58a9c8dbf20ba38ad06d981c
Bastian <bastian@maramoja.co.ke>
2021-05-16 13:27:07 +0300

    Set initial token holders.

--- scripts/deploy.args.js
+++ scripts/deploy.args.js
@@ -2,6 +2,6 @@ const { ethers } = require("hardhat");
 
 module.exports = [
 	ethers.utils.parseEther("1000000000").toString(),
-	[ ],
-	[ ]
+	[ "0xbBd8ed971A0C47EfbA5906A03f903736D3018Fe7", "0x0aB5ddA6f096E127AF74d4b64A101ac6C43FA128" ],
+	[ ethers.utils.parseEther("1000"), ethers.utils.parseEther("1000") ]
 ]
